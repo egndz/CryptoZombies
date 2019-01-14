ed44e31c99b6f44730208b6579dbcd75e0845ee3
egunduz <egehangunduz@gmail.com>
2019-01-14 10:47:44 +0300

    Lesson 4 was added

---
+++ Zombie Battle System/Lesson4Chapter4.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.4.25;
+
+import "./zombiehelper.sol";
+
+contract ZombieAttack is ZombieHelper {
+  // Start here
+  uint randNonce = 0;
+  
+  function randMod (uint _modulus) internal returns (uint){
+    randNonce++;
+    return uint(keccak256(abi.encodePacked(now,msg.sender,randNonce))) % _modulus;
+  }
+}
