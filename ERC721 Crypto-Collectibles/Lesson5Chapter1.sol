7a93fa0a2c317c7c0711587f9b9ae738dad198c5
egunduz <egehangunduz@gmail.com>
2019-01-15 14:55:36 +0300

    Lesson5 was added

---
+++ ERC721 Crypto-Collectibles/Lesson5Chapter1.sol
@@ -0,0 +1,8 @@
+// Start here
+pragma solidity ^0.4.25;
+
+import "./zombieattack.sol";
+
+contract ZombieOwnership is ZombieAttack{
+
+}
---
+++ ERC721 Crypto-Collectibles/Lesson5Chapter2.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.4.25;
+
+import "./zombieattack.sol";
+// Import file here
+import "./erc721.sol";
+
+// Declare ERC721 inheritance here
+contract ZombieOwnership is ZombieAttack, ERC721 {
+
+}
